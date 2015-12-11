32d56cecf4034fdb9b09e160ab6bf19f7c7ac066
Marc Neuhaus <mneuhaus@famelo.com>
2015-12-11 21:30:20 +0100

    added composer.json

--- src/ClassRegister.php
+++ src/ClassRegister.php
@@ -1,12 +1,9 @@
 <?php
 namespace Mia3\Koseki;
 
-/**
- *
- */
 class ClassRegister {
-	public static function getInterfaces($interfaceName) {
-		var_dump($getInterfaces);
-		exit();
+
+	public static function getImplementations($interfaceName) {
+		var_dump($interfaceName);
 	}
 }
